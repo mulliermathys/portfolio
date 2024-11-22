85610c8ea17c1c87adbf800a21418bbf43db74d4
Mathys Mullier <mathys.mullier.etu@univ-lille.fr>
2024-11-22 08:39:46 +0100

    Texte présentation traduction

--- js/app.js
+++ js/app.js
@@ -4,17 +4,9 @@ const navBarElements = document.querySelectorAll('#navElements .content');
 const logo = document.querySelector('#logo');
 const navElements = document.querySelector('#navElements');
 const contact = document.querySelector('#contact');
+const tradButtons = document.querySelectorAll('#presentation .text-container #left-part #trad-button-zone div');
 
-
-function block() {
-  navBar.style.display = 'none';
-  document.querySelector("main").style.display = "none";
-  document.querySelector("header").style.display = "none";
-
-  Swal.fire({title: 'À bientôt :)', text: 'Le site n\'est pas encore terminé. Découvrez-le bientôt !', icon: 'error', confirmButtonText: 'Compris'});
-}
-
-block();
+let headerVisible = true;
 
 /**
  * Fonctions
@@ -34,20 +26,28 @@ function navIncrement() {
     logo.classList.remove('hidden');
     contact.classList.remove('hidden');
     navElements.style.width = '70%';
+    headerVisible = true;
+    console.log("oui")
   }
 }
 
 function windowSizeNav() {
   if (window.innerWidth <= 1150) {
     navReduction();
-  } else navIncrement();
+  } else if (headerVisible) navIncrement();
 }
 
-function activeElement(element) {
-  navBarElements.forEach((elementBis) => {
+function activeElement(allElements, element) {
+  allElements.forEach((elementBis) => {
     if (elementBis.classList.contains('active')) elementBis.classList.remove('active');
   })
   element.classList.add('active');
+}
+
+function switchLanguage(idxLang) {
+  const langVersions = ["Étudiant en BUT Informatique à l'IUT de Lille, je suis spécialisé dans la création d'applications, de leur conception à leur validation. Mon ambition ? Devenir développeur full stack pour concevoir et réaliser des solutions innovantes, ergonomiques et performantes.\n\n Curieux et motivé, je m'intéresse à toutes les facettes du code : des interfaces utilisateur intuitives et attractives aux architectures back-end robustes et fiables. Grâce à ma formation, j'ai acquis de l'expérience pour être efficace à chaque étape du développement d'une application, depuis l'analyse des besoins du client jusqu'à sa livraison pour la rendre entièrement fonctionelle.",
+                "As a student in the Computer Science bachelor's program (BUT Informatique) at IUT de Lille, I specialize in application development, from design to validation. My ambition? To become a full-stack developer, crafting innovative, user-friendly, and high-performance solutions.\n\n Curious and driven, I am passionate about all aspects of coding: from creating intuitive and appealing user interfaces to building robust and reliable back-end architectures. Through my studies, I have gained valuable experience to excel at every stage of application development, from analyzing client needs to delivering fully functional solutions."];
+  document.querySelector("#left-part p").innerHTML = langVersions[idxLang].replace(/\n/g, "<br>");
 }
 
 /**
@@ -59,15 +59,17 @@ document.querySelector('#scrollButton').addEventListener('click', function() {
 });
 
 window.addEventListener('resize', function() {
-  windowSizeNav();
+   if (headerVisible) navIncrement();
+   else navReduction();
 })
 
 window.addEventListener('load', function() {
   windowSizeNav();
+  switchLanguage(0);
 })
 
 navBarElements.forEach((element) => {
-  element.addEventListener('click', function(event) {activeElement(element)});
+  element.addEventListener('click', function(event) {activeElement(navBarElements, element)});
 })
 
 navBarElements.forEach((element) => {
@@ -82,6 +84,14 @@ navBarElements.forEach((element) => {
   })
 })
 
+tradButtons.forEach((element) => {
+  element.addEventListener('click', function(event) {
+    activeElement(tradButtons, element);
+    let buttonsList =  Array.from(tradButtons);
+    switchLanguage(buttonsList.indexOf(element));
+  });
+})
+
 /**
  * Observers
  * */
@@ -92,7 +102,8 @@ const sectionObserver = new IntersectionObserver((entries) => {
       const children = Array.from(entry.target.parentNode.children); // Convertit la NodeList en tableau
       const buttons = Array.from(document.querySelectorAll('#navElements .content'));
       activeElement(buttons[children.indexOf(entry.target)]);
-      navReduction();
+      headerVisible = false;
+      windowSizeNav();
     }
   });
 }, {
@@ -105,7 +116,10 @@ const headerObserver = new IntersectionObserver((entries) => {
       document.querySelectorAll('#navElements .content').forEach((element) => {
         element.classList.remove('active');
       });
+      headerVisible = true;
       navIncrement();
+    } else {
+      navReduction();
     }
   });
 }, {
@@ -117,3 +131,13 @@ document.querySelectorAll("section").forEach((element) => {
 })
 
 headerObserver.observe(document.querySelector('header'));
+
+function block() {
+  navBar.style.display = 'none';
+  document.querySelector("main").style.display = "none";
+  document.querySelector("header").style.display = "none";
+
+  Swal.fire({title: 'À bientôt :)', text: 'Le site n\'est pas encore terminé. Découvrez-le bientôt !', icon: 'error', confirmButtonText: 'Compris'});
+}
+
+//block();
