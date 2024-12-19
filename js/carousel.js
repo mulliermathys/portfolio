0f8f90f61d465de05121caf6e78fa3c626010f45
Mathys Mullier <mathys.mullier.etu@univ-lille.fr>
2024-12-19 23:10:05 +0100

    site done

--- js/carousel.js
+++ js/carousel.js
@@ -3,9 +3,7 @@ const carousel = document.getElementById('carousel');
 const prevBtn = document.getElementById('prev-btn');
 const nextBtn = document.getElementById('next-btn');
 
-
-console.log(document.getElementsByClassName('card')[0].style.width);
-const scrollAmount = 900; // Adjust based on card width and gap
+const scrollAmount = document.getElementsByClassName('card')[0].clientWidth; // Adjust based on card width and gap
 
 prevBtn.addEventListener('click', () => {
   carousel.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
